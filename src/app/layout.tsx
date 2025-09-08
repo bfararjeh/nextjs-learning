42b47a0dc798cfe4687ed9cf90111830d4609a2e
bfararjeh <bahafararjeh@hotmail.com>
2025-09-08 13:12:17 +0100

    Figuring out how NextJS handles fonts
    Either the documentation is wrong or I'm stupid, probably the latter, but I refuse to have an file ref that starts with "../../"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import localFontCenturyGothic from "next/font/local"
 import "./globals.css";
 
 const geistSans = Geist({
@@ -11,6 +12,10 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
+const centuryGothic = localFontCenturyGothic({
+  src: "../../public/fonts/centurygothic.ttf"
+})
 
 export const metadata: Metadata = {
   title: "Create Next App",
