5eb274119f52b37df8cd233d6a9cd5747a664b05
bfararjeh <bahafararjeh@hotmail.com>
2025-09-09 16:55:02 +0100

    Understanding more and more
    Understanding what files are what, removing unnecessary ones, and learning about CSS and what it does/how to use it

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,18 +1,11 @@
-import localFontCenturyGothic from "next/font/local"
-
-const centuryGothic = localFontCenturyGothic({
-  src: "./fonts/centurygothic.ttf"
-});
 
 export default function Home() {
   return (
     <div>
-      <div className={'m-3 ${centuryGothic.className}'}>
+      <div className={'m-8 font-centgoth'}>
         <p> 
           Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla consectetur euismod nibh, eu convallis orci aliquam vel. Phasellus faucibus blandit leo at ultricies. Vivamus venenatis ex gravida finibus posuere. Fusce augue dui, molestie eu commodo nec, dapibus blandit sapien. Nullam fringilla ipsum non sem aliquam tempor. Etiam scelerisque nisl non aliquam mattis. Nullam dapibus, nisl eget tincidunt elementum, purus mi sollicitudin nulla, id rhoncus risus felis luctus risus. Donec interdum, erat ac sollicitudin molestie, dolor ipsum dictum turpis, at congue erat purus in dui. Phasellus ornare sagittis pulvinar.
-        </p>
-
-        <p>
+          <br></br> <br></br>
           Aenean ut justo ipsum. Aenean dignissim id massa varius tempor. Etiam auctor dapibus sodales. Aenean ultricies lacinia risus, volutpat semper lectus pretium eget. Proin at sem nunc. Vestibulum quis magna vitae enim sagittis feugiat ac eu dui. Aenean sollicitudin suscipit tincidunt. Nulla vulputate tristique mi sit amet accumsan. Sed et lacinia lorem, a congue lorem. Duis vitae consequat eros. In porta augue quis nisi eleifend, sed bibendum tellus venenatis. Aenean a ornare libero. Quisque condimentum varius dolor hendrerit aliquet. 
         </p>
       </div>
