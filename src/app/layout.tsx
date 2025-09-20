0e1c8356c214af268992aa5154e2a86620276b6a
bfararjeh <bahafararjeh@hotmail.com>
2025-09-20 17:29:03 +0100

    Neater button row in layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,8 @@ import "./globals.css";
 import Image from "next/image";
 import Link from "next/link";
 
+const buttonClasses = "flex-1 flex items-center gap-3 font-centgoth font-bold justify-center text-stroke-thin text-3xl px-4 py-2 bg-fararjeh text-white transition hover:bg-gradient-to-b hover:from-fararjeh hover:to-fararjeh-dim hover:text-gray-200"
+
 export const metadata: Metadata = {
   title: "Fararjeh domain name",
   description: "Learning Next.js",
@@ -13,17 +15,61 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
   return (
     <html lang="en">
       <body>
-        <header>
-          <Image
-            src={"/fararjeh.png"}
-            alt="Site Icon"
-            width={120}
-            height={120}
-          />
-          <button>
-            <Link href="/" className="font-centgoth font-bold text-8xl tracking-wide text-stroke">Fararjeh</Link>
-          </button>
+        <header className="flex flex-col">
+          <div className="mb-10 flex items-center justify-center gap-6 drop-shadow-[0_0_3px_black]">
+            <Image
+              src="/fararjeh.png"
+              alt="Site Icon"
+              width={128}
+              height={128}
+            />
+            <Link
+              href="/"
+              className="font-centgoth font-bold text-9xl tracking-wide text-stroke"
+            >
+              Fararjeh
+            </Link>
+          </div>
+
+          <nav className={"font-centgoth flex w-full justify-center"}>
+            <a
+              href="https://youtube.com/@fararjeh"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={buttonClasses}
+            >
+              <Image src="/icons/youtube.png" alt="YouTube" width={48} height={48}/>
+              YouTube</a>
+              
+            <a
+              href="https://x.com/balfararjeh"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={buttonClasses}
+            >
+              <Image src="/icons/twitter.png" alt="Twitter" width={48} height={48}/>
+              Twitter</a>
+
+            <a
+              href="https://metafy.gg/@fararjeh"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={buttonClasses}
+            >
+              <Image src="/icons/metafy.png" alt="Metafy" width={48} height={48}/>
+              Metafy</a>
+
+              <a
+              href="https://twitch.tv/fararjeh"
+              target="_blank"
+              rel="noopener noreferrer"
+              className={buttonClasses}
+            >
+              <Image src="/icons/twitch.png" alt="Twitch" width={40} height={32}/>
+              Twitch</a>
+          </nav>
         </header>
+        
         <div className={"m-8"}>{children}</div>
       </body>
     </html>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,47 +1,8 @@
-import Image from "next/image";
-
-const buttonClasses = "flex items-center gap-2 font-centgoth font-bold justify-center text-stroke-thin text-3xl px-4 py-2 bg-fararjeh text-white shadow-md rounded-xl transition"
 
 export default function Home() {
   return (
     <div>
-      <div className={"m-8 font-centgoth flex items-center justify-center gap-8"}>
-          <a
-            href="https://youtube.com/@fararjeh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${buttonClasses} hover:bg-fararjeh-dim hover:text-gray-200`}
-          >
-            <Image src="/icons/youtube.png" alt="YouTube" width={48} height={48}/>
-            YouTube</a>
-            
-          <a
-            href="https://x.com/balfararjeh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${buttonClasses} hover:bg-fararjeh-dim hover:text-gray-200`}
-          >
-            <Image src="/icons/twitter.png" alt="Twitter" width={48} height={48}/>
-            Twitter</a>
-
-          <a
-            href="https://metafy.gg/@fararjeh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${buttonClasses} hover:bg-fararjeh-dim hover:text-gray-200`}
-          >
-            <Image src="/icons/metafy.png" alt="Metafy" width={48} height={48}/>
-            Metafy</a>
-
-            <a
-            href="https://twitch.tv/fararjeh"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={`${buttonClasses} hover:bg-fararjeh-dim hover:text-gray-200`}
-          >
-            <Image src="/icons/twitch.png" alt="Twitch" width={40} height={32}/>
-            Twitch</a>
-      </div>
+      page
     </div>
   );
 }
