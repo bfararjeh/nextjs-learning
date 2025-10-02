2f4b5d29f1b1065bf14fca3b30ad77e9bc87ee09
Baha Alfararjeh <bahafararjeh@hotmail.com>
2025-10-02 13:58:26 +0100

    Site overhaul
    Mainly aesthetics, padding and a border change everything

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,96 +12,94 @@ export default function Home() {
   const opacity3 = useTransform(scrollYProgress, [0.75, 0.751], [0, 1]);
   
   return (
-    <div>
+    <div className="flex flex-row">
+      
+      <aside className="w-325 px-10 py-10 flex flex-col top-27 sticky h-screen justify-center items-center border-r">
+        <h2 className="text-2xl font-bold mb-6">Sidebar</h2>
+        <nav className="flex flex-col gap-4">
+          <p>section 1</p>
+          <p>section 2</p>
+          <p>section 3</p>
+        </nav>
+      </aside>
 
-      <header className="flex flex-col">
-        <div className="mb-10 flex items-center justify-center gap-6 drop-shadow-[0_0_3px_black]">
-          <Image
-            src="/icons/fararjeh.png"
-            alt="Site Icon"
-            width={128}
-            height={128}
-          />
-          <Link
-            href="/"
-            className="font-centgoth font-bold text-9xl tracking-wide text-stroke"
-          >
-            Fararjeh
-          </Link>
+
+      <div className="px-75">
+        <Image
+          src="/home/showcase_1.png"
+          alt="showcase"
+          width={900}
+          height={1}
+          quality={100}
+          className="mt-20 rounded-4xl m-auto border-2 border-gray-200"
+        />
+
+        <div>
+          <p className="header-xl mt-15">Lorem Ipsum Dolor Sit Amet</p>
+          <p className="paragraph mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu auctor neque, eu rhoncus massa. Donec malesuada orci a ex blandit pretium. Proin a porttitor neque, non posuere massa. Nullam id varius leo. Vivamus dapibus, nibh eget facilisis iaculis, nibh odio viverra nunc, et mattis nibh neque sed dolor. Aliquam ante tellus, rhoncus pellentesque justo sed, bibendum placerat metus. Nam velit mi, posuere nec sem eu, interdum sodales turpis. Curabitur a massa non magna tempor auctor. Nunc enim erat, luctus ac est ut, blandit ultrices sem. Aenean placerat vel turpis sollicitudin fringilla. Aenean vel odio id elit suscipit ultrices non ac arcu. </p>
         </div>
-      </header>
 
-      <div>
-        <p className="header mt-20">Competitor. Coach. Content Creator.</p>
-        <p className="paragraph mt-20">I am Fararjeh. I am a competitor, coach, and content creator specialising in Street Fighter 6. You may know me from my YouTube channel, met me in person at an event, or signed up for some of my coaching on Metafy.<br/><br/>
-        My goal is to be the best. Juggling my prescence in the community with my trainign is hard, but not impossible. If you'd like to learn more about me, feel free to scroll around the site.
-        </p>
-      </div>
-
-      <div className="flex flex-col justify-between items-center px-12 py-20 gap-10">
-        <motion.div style={{ opacity: opacity1 }} className="transition-opacity duration-700 flex">
-          <div>
-            <Image
-              src="/home/fighting-games.jpg"
-              alt="FGC"
-              width={1250}
-              height={1250}
-              quality={100}
-              className="image"
-            />
-          </div>
-
-          <div className="flex flex-col justify-center">
-            <p className="header">A Competitor.</p>
-            <p className="paragraph mt-20">Street Fighter 6 is my first serious fighting game, and since it's release I have been competing in online and offline events ranging from the international level to the local level. Each year I get stronger and stronger; I know I'll make it, it's just a matter of time for me.<br/><br/>
-            Although this CPT season has been hectic for me with my graduation and all, I still made time to travel to events such as UFA and Brussels Challenge. You can take a look at some of my placements below.</p>
-          </div>
-        </motion.div>
-
-        <motion.div style={{ opacity: opacity2 }} className="transition-opacity duration-700 flex">
-          <div className="flex flex-col justify-center">
-            <p className="header">A Coach.</p>
-            <p className="paragraph mt-20">As of August 2025, I have been coaching players of any level on my Metafy. I have a natural talent for breaking down complex topics and solving problems that I've been mastering over the last few years in all other aspects of my life.<br/><br/>
-            Proof? My video, "You Can't Play Neutral" is the first result when you search up "Street Fighter 6 Neutral" on YouTube. More proof? Click below to take a look at some of my testimonials from Metafy.</p>
-          </div>
-
-          <div>
-            <Image
-              src="/home/status.png"
-              alt="Metafy"
-              width={1250}
-              height={1250}
-              quality={100}
-              className="image"
-            />
-          </div>
-        </motion.div>
-
-        <motion.div style={{ opacity: opacity3 }} className="transition-opacity duration-700 flex">
-
-          <div className="relative">
-            <Image
-              src="/home/sub-count-test.png"
-              alt="Sub Counter"
-              width={1250}
-              height={1250}
-              quality={100}
-              className="image"
-            />
-
-            <div className="mb-5 ml-1 absolute inset-0 flex flex-col justify-center text-center text-[7.5em] text-black font-sans font-semibold">
-                <ReturnSubs />
+        <div className="flex flex-col justify-between items-center px-12 py-20 gap-10">
+          <motion.div style={{ opacity: opacity1 }} className="transition-opacity duration-700 flex">
+            <div>
+              <Image
+                src="/home/fighting-games.jpg"
+                alt="FGC"
+                width={3000}
+                height={1}
+                quality={100}
+                className="image"
+              />
             </div>
 
-          </div>
+            <div className="flex flex-col justify-center">
+              <p className="header">Lorem Ipsum</p>
+              <p className="paragraph mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu auctor neque, eu rhoncus massa. Donec malesuada orci a ex blandit pretium. Proin a porttitor neque, non posuere massa. Nullam id varius leo. Vivamus dapibus, nibh eget facilisis iaculis, nibh odio viverra nunc, et mattis nibh neque sed dolor. Aliquam ante tellus, rhoncus pellentesque justo sed, bibendum placerat metus. Nam velit mi, posuere nec sem eu, interdum sodales turpis. Curabitur a massa non magna tempor auctor. Nunc enim erat, luctus ac est ut, blandit ultrices sem. Aenean placerat vel turpis sollicitudin fringilla. Aenean vel odio id elit suscipit ultrices non ac arcu. </p>
+            </div>
+          </motion.div>
 
-          <div className="flex flex-col justify-center">
-            <p className="header">A Content Creator.</p>
-            <p className="paragraph mt-20">From the moment I posted my first video, an Elden Ring PvP Montage in April of 2022, I knew I loved making videos. Once Street Fighter 6 came out I decided to make a video on Neutral, a grossly misunderstood topic, and from that point the rest is history.<br/><br/>
-            From educational tutorials to clip montages and everything inbetween, it's all on my YouTube. You can read more about how I started my channel here, or just go watch my biggest video: You Can't Play Neutral.</p>
-          </div>
-        </motion.div>
+          <motion.div style={{ opacity: opacity2 }} className="transition-opacity duration-700 flex">
+            <div className="flex flex-col justify-center">
+              <p className="header">Lorem Ipsum</p>
+              <p className="paragraph mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu auctor neque, eu rhoncus massa. Donec malesuada orci a ex blandit pretium. Proin a porttitor neque, non posuere massa. Nullam id varius leo. Vivamus dapibus, nibh eget facilisis iaculis, nibh odio viverra nunc, et mattis nibh neque sed dolor. Aliquam ante tellus, rhoncus pellentesque justo sed, bibendum placerat metus. Nam velit mi, posuere nec sem eu, interdum sodales turpis. Curabitur a massa non magna tempor auctor. Nunc enim erat, luctus ac est ut, blandit ultrices sem. Aenean placerat vel turpis sollicitudin fringilla. Aenean vel odio id elit suscipit ultrices non ac arcu. </p>
+            </div>
 
+            <div>
+              <Image
+                src="/home/status.png"
+                alt="Metafy"
+                width={3000}
+                height={1}
+                quality={100}
+                className="image"
+              />
+            </div>
+          </motion.div>
+
+          <motion.div style={{ opacity: opacity3 }} className="transition-opacity duration-700 flex">
+
+            <div className="relative">
+              <Image
+                src="/home/sub-count-test.png"
+                alt="Sub Counter"
+                width={3000}
+                height={1}
+                quality={100}
+                className="image"
+              />
+
+              <div className="mt-1 ml-1 absolute inset-0 flex flex-col justify-center text-center text-[5.5em] text-black font-sans font-semibold">
+                  <ReturnSubs />
+              </div>
+
+            </div>
+
+            <div className="flex flex-col justify-center">
+              <p className="header">Lorem Ipsum</p>
+              <p className="paragraph mt-20">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eu auctor neque, eu rhoncus massa. Donec malesuada orci a ex blandit pretium. Proin a porttitor neque, non posuere massa. Nullam id varius leo. Vivamus dapibus, nibh eget facilisis iaculis, nibh odio viverra nunc, et mattis nibh neque sed dolor. Aliquam ante tellus, rhoncus pellentesque justo sed, bibendum placerat metus. Nam velit mi, posuere nec sem eu, interdum sodales turpis. Curabitur a massa non magna tempor auctor. Nunc enim erat, luctus ac est ut, blandit ultrices sem. Aenean placerat vel turpis sollicitudin fringilla. Aenean vel odio id elit suscipit ultrices non ac arcu. </p>
+            </div>
+          </motion.div>
+        </div>
       </div>
     </div>
   );
