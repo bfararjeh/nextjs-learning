4ed2bea60b39398c8ba5fd7504a665754a642e25
Baha Alfararjeh <bahafararjeh@hotmail.com>
2025-10-30 19:47:42 +0000

    Update route.ts

--- src/app/api/sitemap-images/route.ts
+++ src/app/api/sitemap-images/route.ts
@@ -7,24 +7,31 @@ export async function GET(req: NextRequest) {
   const urls = galleryImages
     .map((img) => {
       const fileName = img.split("/").pop()?.split(".")[0] || "";
-      const caption = fileName.replace(/[-_]+/g, " ").trim();
+      const escapeXml = (unsafe: string) =>
+        unsafe.replace(/&/g, "&amp;")
+          .replace(/</g, "&lt;")
+          .replace(/>/g, "&gt;")
+          .replace(/"/g, "&quot;")
+          .replace(/'/g, "&apos;");
+      
+      const caption = escapeXml(fileName.replace(/[-_]+/g, " ").trim());
 
       return `
-      <url>
-        <loc>${baseUrl}/#gallery</loc>
-        <image:image>
-          <image:loc>${baseUrl}${img}</image:loc>
-          <image:caption>${caption}</image:caption>
-        </image:image>
-      </url>`;
+        <url>
+          <loc>${baseUrl}/</loc>
+          <image:image>
+            <image:loc>${baseUrl}${img}</image:loc>
+            <image:caption>${caption}</image:caption>
+          </image:image>
+        </url>`;
     })
     .join("");
 
-  const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
-  <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
-          xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
+    const sitemap = `<?xml version="1.0" encoding="UTF-8"?>
+    <urlset xmlns="http://www.sitemaps.org/schemas/sitemap/0.9"
+            xmlns:image="http://www.google.com/schemas/sitemap-image/1.1">
     ${urls}
-  </urlset>`;
+    </urlset>`;
 
   return new Response(sitemap, {
     headers: {
