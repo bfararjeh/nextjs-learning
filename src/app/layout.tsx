3bcd740bdb90231f1bd417aad2c10880df745130
Baha Alfararjeh <bahafararjeh@hotmail.com>
2025-11-01 15:19:54 +0000

    Added vercel's built-in analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import { Analytics } from "@vercel/analytics/next";
 import "./globals.css";
 import Link from "next/link";
 import InsertHeader from "../components/InsertHeader";
@@ -47,6 +48,8 @@ export default function RootLayout({children,}: Readonly<{children: React.ReactN
         </div>
 
         <InsertFooter/>
+
+        <Analytics />
       </body>
     </html>
   );
